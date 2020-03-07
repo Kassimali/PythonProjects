ad6244334b5e459000578ae68352badea64624fb
Kassim Mohamed <kassimali114@gmail.com>
2020-03-07 11:12:44 +0300

    first commit

---
+++ demo.py
@@ -0,0 +1,10 @@
+
+dic={"Name":"Kassim Ali","Age":31,"Name":"Ahmed Mohamed","Age":51}
+
+
+for key,value in dic.items():
+    print("Key")
+    print(key)
+    print("Value")
+    print(value)
+    print()
